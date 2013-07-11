8b4d3e66e74f85042728888dd30f329feaea94df
David Goodlad <david@goodlad.net>
2013-07-12 08:57:51 +1000

    Update to upstream version 2013.5.20
    This version includes support for Mail.app on Mountain Lion

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include gpgtools
 class gpgtools {
   package { 'gpgtools':
-    source   => 'https://github.com/downloads/GPGTools/GPGTools/GPGTools-20120318.dmg',
+    source   => 'https://s3.amazonaws.com/gpgtools/GPGTools-2013.5.20.dmg',
     provider => 'pkgdmg'
   }
 }
